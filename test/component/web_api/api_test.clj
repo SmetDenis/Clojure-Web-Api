9ac4159a58a96d8ceb218d61d181ed380518dcdf
Denis Smet <denis.smetannikov@yandex.ru>
2023-10-10 19:04:43 +0400

    http test

--- test/component/web_api/api_test.clj
+++ test/component/web_api/api_test.clj
@@ -1,5 +1,27 @@
 (ns component.web-api.api-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [com.stuartsierra.component :as component]
+            [web-api.core :as core]
+            [clj-http.client :as client]))
+
+(defmacro with-system
+  [[bound-var binding-expr] & body]
+  `(let [~bound-var (component/start ~binding-expr)]
+     (try
+       ~@body
+       (finally
+         (component/stop ~bound-var)))))
+
+(deftest greeting-test
+  (with-system
+    [sut (core/web-api-system {:server {:port 8088}})]
+    (is (= {:body   "Hello, world!"
+            :status 200}
+           (->
+             (str "http://localhost:" 8088 "/greet")
+             (client/get)
+             (select-keys [:body :status])
+             )))))
 
 (deftest a-simple-api-test
   (is (= 1 1)))
